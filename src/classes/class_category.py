b49a94dc25f1a0a9363406e2d0984002ce82b709
IvanPro91 <ivan-don4encko@ya.ru>
2025-04-03 19:12:51 +0300

    Написана логика для работы с Категориями и тесты к ним

--- src/classes/class_category.py
+++ src/classes/class_category.py
@@ -1,13 +1,55 @@
+from typing import Any
+
 from src.classes.class_products import Product
 
 
 class Category:
     category_count: int = 0
     product_count: int = 0
+    __products: list[Product] = []
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
-        self.products = products
         self.category_count += 1
-        self.product_count += len(products)
+
+    @classmethod
+    def add_product(cls, product: Product) -> None:
+        """Добавление нового продукта"""
+        # Проверить продукт на цену и произвести поиск
+        quantity, price = cls.chk_product(product)
+        product.quantity = quantity
+        product.price = price
+
+        cls.__products.append(product)
+        cls.product_count = product.quantity
+
+    @classmethod
+    def chk_product(cls, product: Product) -> Any:
+        """Проверка добавляемых данных"""
+        # Проверка на схожесть в имени IN
+        quantity = 1
+        price = product.price
+
+        # Ищем продукт с таким-же именем
+        for all_product in cls.__products:
+            if product.name == all_product.name:
+                quantity = product.quantity + all_product.quantity
+
+                # Решаем конфликт цен
+                if product.price > all_product.price:
+                    price = product.price
+                else:
+                    success_low_price = input(f"Цена товара {product.name} снижена, продолжить?\n")
+                    if success_low_price.lower() == "y":
+                        price = product.price
+
+        return quantity, price
+
+    @property
+    def products(self) -> str:
+        """Сеттер, который возвращает информацию по продуктам"""
+        all_products = "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n" for product in self.__products]
+        )
+        return all_products
--- tests/classes/test_class_category.py
+++ tests/classes/test_class_category.py
@@ -1,48 +1,90 @@
-import pytest
+from unittest.mock import patch
+
+from _pytest.capture import CaptureFixture
 
 from src.classes.class_category import Category
 from src.classes.class_products import Product
 
 
-@pytest.mark.parametrize(
-    "name, description, products, result",
-    [
-        ("Тест 1", "Описание 1", [], Category),
-        ("Тест 2", "Описание 2", [], Category),
-    ],
-)
-def test_category(name: str, description: str, products: list, result: Category) -> None:
+def test_category(product_phone: Product) -> None:
     """
-    Тестирование класса Category по заготовленным данным
-    :param name: Название
-    :param description: Описание
-    :param products: Список Product
-    :param result: Ожидание от теста
-    :return: None
+    Тестирование класса Category на добавление новой продукции
     """
+    category = Category("Категория 1", "Описание 1")
+    category.add_product(product_phone)
 
-    product = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category = Category(name, description, [product])
-
-    assert isinstance(category, Category)
+    assert category.category_count == 1
+    assert category.product_count == 1
 
 
-def test_category_init() -> None:
+def test_category_price(capsys: CaptureFixture[str], product_phone: Product) -> None:
     """
-    Тестирование Category
-    :return: None
+    Тестирование проверки добавления продукции с разными ценами
     """
+    category = Category("Категория 1", "Описание 1")
 
-    product = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category = Category("Тест 1", "Описание 1", [product])
+    with patch("builtins.input", lambda _: "y"):
+        category.add_product(product_phone)
+        product_phone.price = 12322
+        category.add_product(product_phone)
+        read_out = capsys.readouterr()
+        assert read_out.out == ""
 
-    assert category.name == "Тест 1"
-    assert category.description == "Описание 1"
-    assert category.products == [product]
-    assert len(category.products) == 1
+    with patch("builtins.input", lambda _: "n"):
+        category.add_product(product_phone)
+        product_phone.price = 11322
+        category.add_product(product_phone)
+        read_out = capsys.readouterr()
+        assert read_out.out == ""
 
-    get_first_product = category.products[0]
-    assert get_first_product.name == '55" QLED 4K'
-    assert get_first_product.description == "Фоновая подсветка"
-    assert get_first_product.price == 123000.0
-    assert get_first_product.quantity == 7
+
+def test_category_quantity(capsys: CaptureFixture[str], product_phone: Product) -> None:
+    """
+    Тестирование проверки добавления продукции по количеству
+    """
+    category = Category("Категория 1", "Описание 1")
+
+    with patch("builtins.input", lambda _: "y"):
+        category.add_product(product_phone)
+        product_phone.quantity = 10
+        category.add_product(product_phone)
+        category.add_product(product_phone)
+        assert category.product_count == 40
+
+    with patch("builtins.input", lambda _: "n"):
+        category.add_product(product_phone)
+        product_phone.quantity = 10
+        category.add_product(product_phone)
+        assert category.product_count == 20
+
+
+def test_get_products(product_phone: Product) -> None:
+    """
+    Тестирование проверки добавления продукции по количеству
+    """
+    with patch("builtins.input", lambda _: "y"):
+        category = Category("Категория 1", "Описание 1")
+        category.add_product(product_phone)
+        assert category.products == (
+            "Iphone 15, 210000.0 руб. Остаток: 1 шт.\n"
+            "\n"
+            "Iphone 15, 11322 руб. Остаток: 72 шт.\n"
+            "\n"
+            "Iphone 15, 11322 руб. Остаток: 72 шт.\n"
+            "\n"
+            "Iphone 15, 11322 руб. Остаток: 72 шт.\n"
+            "\n"
+            "Iphone 15, 11322 руб. Остаток: 72 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 20 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 20 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 20 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 20 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 20 шт.\n"
+            "\n"
+            "Iphone 15, 210000.0 руб. Остаток: 28 шт.\n"
+        )
