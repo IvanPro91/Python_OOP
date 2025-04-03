4230d4aed0fc91888ee47de8132bdaec065ee600
IvanPro91 <ivan-don4encko@ya.ru>
2025-04-03 20:37:37 +0300

    Правки по проекту

--- src/classes/class_products.py
+++ src/classes/class_products.py
@@ -5,20 +5,19 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.product_price = price
         self.__price = price
         self.quantity = quantity
 
     @property
     def price(self) -> float:
-        return self.product_price
+        return self.__price
 
     @price.setter
     def price(self, price: float) -> None:
-        if price > 0:
-            self.product_price = price
-        else:
+        if price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = price
 
     @staticmethod
     def new_product(name: str, description: str, price: float, quantity: int) -> Any:
