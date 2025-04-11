41dc9bd5a924e7df68c7a6726c464c4bbfcb324e
IvanPro91 <ivan-don4encko@yandex.ru>
2025-04-12 00:01:17 +0300

    Исправления фикстур и возврат функции чтения файлов

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.classes.class_category import Category
 from src.classes.class_products import Product
 
 
@@ -10,3 +11,20 @@ def product_phone() -> Product:
     :return: Product
     """
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def fix_category() -> Category:
+    """
+    Фикстура для Category
+    :return: Category
+    """
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Apple iPhone 14 Pro", "128GB, Золотой цвет, 48MP камера", 120000.0, 10)
+    product3 = Product("Google Pixel 7 Pro", "128GB, Белый цвет, 50MP камера", 90000.0, 7)
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+    return category1
