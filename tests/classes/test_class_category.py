55d1c0aaaae5baebd715478550343aad713cf79f
IvanPro91 <ivan-don4encko@yandex.ru>
2025-04-12 10:20:30 +0300

    Добавлен класс Order наследуемый от абстрактного класса BaseCategory и тесты к ним

--- tests/classes/test_class_category.py
+++ tests/classes/test_class_category.py
@@ -1,8 +1,9 @@
 from unittest.mock import patch
 
+import pytest
 from _pytest.capture import CaptureFixture
 
-from src.classes.class_category import Category
+from src.classes.class_category import Category, Order
 from src.classes.class_products import Product
 
 
@@ -28,7 +29,10 @@ def test_category_price(capsys: CaptureFixture[str], product_phone: Product) -> 
         product_phone.price = 12322
         category.add_product(product_phone)
         read_out = capsys.readouterr()
-        assert read_out.out == ""
+        assert read_out.out == (
+            "Создан объект класса Product с параметрами: name='Iphone 15' "
+            "description='512GB, Gray space' price=210000.0 quantity=8\n"
+        )
 
     with patch("builtins.input", lambda _: "n"):
         category.add_product(product_phone)
@@ -73,3 +77,26 @@ def test_str_category(fix_category: Category) -> None:
     Тестирование переопределения метода str
     """
     assert isinstance(str(fix_category), str)
+
+
+def test_order_abs_category(fix_category: Category) -> None:
+    """
+    Тестирование переопределения метода str
+    """
+    product1 = Product("Продукт1", "Описание продукта", 1200, 10)
+    product2 = Product("Продукт2", "Описание продукта", 800, 5)
+
+    # Создание категории и добавление продуктов
+    category = Category("Категория1", "Описание категории", [])
+    category.add_product(product1)
+    category.add_product(product2)
+    print(category)
+
+    order = Order(product1)
+    assert str(order) == "Продукт1"
+    assert order.get_total_cost() == 1200
+
+    order.add_product(product2)
+
+    with pytest.raises(ValueError):
+        order.add_product("Не продукт")
